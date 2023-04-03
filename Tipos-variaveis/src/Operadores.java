39933a34683285165c57953c0def4a23394f6731
oMarcoMaciel <marcomaciel06@gmail.com>
2023-04-03 09:06:49 -0300

    Fim da aula de operadores

---
+++ Tipos-variaveis/src/Operadores.java
@@ -0,0 +1,25 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        /* Após perceber uma String a IDE passa a concatenar,
+        ou seja, a partir daquele caracter será considerado 
+        tudo como string.
+         */
+
+         int a, b;
+         a = 5;
+         b = 6;
+
+         String resultado = a==b ? "Verdadeiro" : "falso";
+         
+        System.out.println(resultado);
+ 
+        //Maneira simplificada p/ booleano
+
+        
+         
+    }
+}
