d5344da94e8a3a5ea3c4457fedcdbf79bb4633be
oMarcoMaciel <marcomaciel06@gmail.com>
2023-04-03 07:59:37 -0300

    Corrigindo erros de digitação

--- Tipos-variaveis/src/TiposVariaveis.java
+++ Tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,6 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! Today is 04/04/2023");
     }
 }
+
